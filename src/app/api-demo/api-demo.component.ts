d3bfaf4d77c6e6368fe54e871189b1dbab6d87a8
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-07-01 09:34:57 +0800

    fetching & displaying data from API

--- src/app/api-demo/api-demo.component.ts
+++ src/app/api-demo/api-demo.component.ts
@@ -7,15 +7,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./api-demo.component.css']
 })
 export class ApiDemoComponent implements OnInit {
+  // Array to store the retrieved data from the mock api
   mockAPI: any[] = [];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    // Make an HTTP GET request to the specified API URL
       this.http.get<any[]>('https://mocki.io/v1/ff6c51fb-193e-4aba-bbc9-c6795f69e376')
           .subscribe(data => {
-            console.log(data)
-            this.mockAPI = data;
+            this.mockAPI = data;  // Assign the received data to the array
           })
   }
 }
